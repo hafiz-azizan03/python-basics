7f2993afb82e3c71feb1b2b9a1f89aa8075fa91f
hafiz-azizan03 <139707724+hafiz-azizan03@users.noreply.github.com>
2025-02-04 15:27:03 +0300

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,17 @@
+operator=input("Enter an operator(+ - * /)")
+num1= float(input("Enter 1st number:"))
+num2= float(input("Enter 2nd number:"))
+
+if operator == "+":
+    result= num1 + num2
+    print(result)
+elif operator == "-":
+    result = num1 - num2
+    print(result)
+elif operator == "*":
+    result = num1 * num2
+    print(result)
+elif operator== "/":
+    result = num1 / num2
+    print(result)
+
