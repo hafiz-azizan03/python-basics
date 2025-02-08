e4fb4d6323acde5fc17f834642d27a8aff3a8ae7
hafiz-azizan03 <139707724+hafiz-azizan03@users.noreply.github.com>
2025-02-08 16:41:38 +0300

    Create dice_game.py

---
+++ dice_game.py
@@ -0,0 +1,58 @@
+import random
+
+#print("\u25CF \u250C \u2500 \u2510 \u2502 \u2514 \u2518") #7 different Unicode respectively
+#● ┌ ─ ┐ │ └ ┘ this is needed to produce ACSII art (American Standard Code for Information Interchange)
+
+dice_art = {
+    1: ("┌─────────┐",
+        "│         │",
+        "│    ●    │",
+        "│         │",
+        "└─────────┘"),
+    2: ("┌─────────┐",
+        "│  ●      │",
+        "│         │",
+        "│      ●  │",
+        "└─────────┘"),
+    3: ("┌─────────┐",
+        "│  ●      │",
+        "│    ●    │",
+        "│      ●  │",
+        "└─────────┘"),
+    4: ("┌─────────┐",
+        "│  ●   ●  │",
+        "│         │",
+        "│  ●   ●  │",
+        "└─────────┘"),
+    5: ("┌─────────┐",
+        "│  ●   ●  │",
+        "│    ●    │",
+        "│  ●   ●  │",
+        "└─────────┘"),
+    6: ("┌─────────┐",
+        "│  ●   ●  │",
+        "│  ●   ●  │",
+        "│  ●   ●  │",
+        "└─────────┘")
+}
+
+dice = []
+total = 0
+num_of_dice = int(input("How many dice?: ")) #make sure it is an integer
+
+for die in range(num_of_dice): #For each die we want to roll (up to the total number of dice we specified) *die is singular noun for dice 
+    dice.append(random.randint(1, 6)) # 1)generate a num from 1-6,add that value into the dice
+
+#for die in range(num_of_dice): #this outer loop will be in chaerged for num of dice
+#    for line in dice_art.get(dice[die]): #this inner for loop will be in charged for printing tuple. 1) die value is taken from user input(example 3) 2) dice_art.get(3) will get the 3rd dice art and stores it in line
+#       print(line)
+
+for line in range(5):
+    for die in dice:
+        print(dice_art.get(die)[line], end="")
+    print()
+
+
+for die in dice:
+    total += die
+print(f"Total: {total}")
