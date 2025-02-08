d4dc00f29f013c44631a551a2592a1320992d01b
hafiz-azizan03 <139707724+hafiz-azizan03@users.noreply.github.com>
2025-02-08 20:05:11 +0300

    Update dice_game.py

--- dice_game.py
+++ dice_game.py
@@ -47,9 +47,9 @@ for die in range(num_of_dice): #For each die we want to roll (up to the total nu
 #    for line in dice_art.get(dice[die]): #this inner for loop will be in charged for printing tuple. 1) die value is taken from user input(example 3) 2) dice_art.get(3) will get the 3rd dice art and stores it in line
 #       print(line)
 
-for line in range(5):
-    for die in dice:
-        print(dice_art.get(die)[line], end="")
+for line in range(5): #For each line (from 0 to 4) of the ASCII art representation of the dice
+    for die in dice: #for each die roll in our dice list(lets say we have 3 die roll with values 3,5,1)
+        print(dice_art.get(die)[line], end="") #.get the ASCII art for that die value, then get the specific line of that art which are 3,5 and 1 (based on the outer loop), and print that line without adding a newline character.
     print()
 
 
