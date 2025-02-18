332e616137a3a665f2b881610690020c545541eb
hafiz-azizan03 <139707724+hafiz-azizan03@users.noreply.github.com>
2025-02-18 20:52:31 +0300

    Update notes_2.py

--- notes_2.py
+++ notes_2.py
@@ -757,7 +757,45 @@
 #else:
 #    print("The target date hasn't passed")
 
+#-------------------------------------------- multithreading --------------------------------------------
 
+# multithreading = Used to perform multiple tasks concurrently (multitasking)
+#                   Good for I/O bound tasksIlike reading files or fetching data from APIs
+#                   threading. Thread(target=my_function)
+
+#import threading
+#import time
+
+#def walk_dog(first, last) :
+#    time. sleep(8)
+#    print(f"You finish walking {first} {last}")
+#def take_out_trash() :
+#    time. sleep(2)
+#    print("You take out the trash")
+#def get_mail():
+#    time. sleep(4)
+#    print("You get the mail")
+
+#chore1 = threading. Thread(target=walk_dog, args=("Scooby", "Doo"))  #the args are tuples, so if we only have 1 tuple, then we would write ("Scooby",)
+#chore1. start ()
+
+#chore2 = threading. Thread(target=take_out_trash)
+#chore2. start ()
+
+#chore3 = threading. Thread(target=get_mail)
+#chore3 . start()
+
+#chore1. join()  #the join() method is used to ensure that the main program waits for each thread to complete its execution before moving on to the next part of the code.
+#chore2. join()
+#chore3. join()
+
+#print("All chores are complete!")
+
+#results:
+#You take out the trash
+#You get the mail
+#You finish walking Scooby Doo
+#All chores are complete!
 
 
 
