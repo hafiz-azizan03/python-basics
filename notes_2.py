2cc60b483df03d2f2d7b45638855d7a39a2e9011
hafiz-azizan03 <139707724+hafiz-azizan03@users.noreply.github.com>
2025-02-18 20:20:19 +0300

    Update notes_2.py

--- notes_2.py
+++ notes_2.py
@@ -739,23 +739,23 @@
 
 #-------------------------------------------- dates and time --------------------------------------------
 
-import datetime
+#import datetime
 
-date = datetime.date(2025, 1, 2)  #access the datetime module and use date method
-today = datetime.date(2025,2,18)
-time = datetime.time(12,30,0)
-now = datetime.datetime.now()  #access the datetime module, datetime class adn the now method, this will read what the system's time now 
+#date = datetime.date(2025, 1, 2)  #access the datetime module and use date method
+#today = datetime.date(2025,2,18)
+#time = datetime.time(12,30,0)
+#now = datetime.datetime.now()  #access the datetime module, datetime class adn the now method, this will read what the system's time now 
 
-now = now.strftime("%H:%M:%S %d %m %y")    #strftime - string format time #the percent specifiers helps the appearanceof the time and date
+#now = now.strftime("%H:%M:%S %d %m %y")    #strftime - string format time #the percent specifiers helps the appearanceof the time and date
 
-target_datetime = datetime.datetime(2030,1,2 ,12,30,1)
-current_datetime = datetime.datetime.now()
+#target_datetime = datetime.datetime(2030,1,2 ,12,30,1)
+#current_datetime = datetime.datetime.now()
 
-if target_datetime<current_datetime:
-    print("The target date has passed")
+#if target_datetime<current_datetime:
+#    print("The target date has passed")
 
-else:
-    print("The target date hasn't passed")
+#else:
+#    print("The target date hasn't passed")
 
 
 
