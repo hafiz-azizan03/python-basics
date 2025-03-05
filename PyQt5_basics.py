0a4f6ad597ee97b9cb99aae3147e112d64a8bc81
hafiz-azizan03 <139707724+hafiz-azizan03@users.noreply.github.com>
2025-03-05 18:26:41 +0300

    Update PyQt5_basics.py

--- PyQt5_basics.py
+++ PyQt5_basics.py
@@ -1,6 +1,7 @@
 #PyQt5 intro
 # PyQt5 layouts
 #PyQt5 BUTTONS
+# PyQt5 Checkboxes
 
 
 
@@ -151,3 +152,39 @@ sys. exit (app.exec())
 
 
 
+
+
+
+
+
+
+# PyQt5 Checkboxes
+import sys
+from PyQt5. QtWidgets import QApplication, QMainWindow, QCheckBox
+from PyQt5. QtCore import Qt
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(700, 300, 500, 500)
+        self.checkbox = QCheckBox("Do you like food?",self)
+        self.initUI()
+
+    def initUI (self) :
+        self.checkbox.setGeometry(10, 0, 500, 100)
+        self.checkbox.setStyleSheet ("font-size: 30px; "
+                                        "font-family: Arial; ")
+        self.checkbox.setChecked(False)
+        self.checkbox.stateChanged.connect (self.checkbox_changed)
+    def checkbox_changed (self, state) :
+        if state == Qt. Checked:
+            print("You like food")
+        else:
+            print("You DO NOT like food")
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+sys. exit (app.exec_())
