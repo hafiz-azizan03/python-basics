1bd95fa54c0710caf82cf9e64b9db4bc2aa8d290
hafiz-azizan03 <139707724+hafiz-azizan03@users.noreply.github.com>
2025-03-05 17:41:20 +0300

    Update PyQt5_basics.py

--- PyQt5_basics.py
+++ PyQt5_basics.py
@@ -1,4 +1,9 @@
- #PyQt5 intro
+#PyQt5 intro
+# PyQt5 layouts
+
+
+
+#PyQt5 intro
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow,QLabel
 from PyQt5.QtGui import QIcon
@@ -54,3 +59,65 @@ def main():
 
 if __name__ == "__main__":
     main()                                          #in case we nee to run this program directly,then just invoke the function main
+
+
+
+# PyQt5 layouts
+import sys
+from PyQt5. QtWidgets import (QApplication, QMainWindow, QLabel,
+                            QWidget, QVBoxLayout, QHBoxLayout, QGridLayout)
+
+
+class MainWindow(QMainWindow) :
+    def __init__(self):
+        super() .__init__()
+        self.setGeometry(700, 300, 500, 500) 
+        self.initUI()
+
+
+    def initUI (self):
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
+
+        label1=QLabel("#1",self)
+        label2=QLabel("#2",self)
+        label3=QLabel("#3",self)
+        label4=QLabel("#4",self)
+        label5=QLabel("#5",self)
+
+        vbox=QVBoxLayout()       #vbox means vertical box, hbox horizontal, grid gives grid but must insert row and collumn
+    
+
+        vbox.addWidget(label1)
+        vbox.addWidget(label2)
+        vbox.addWidget(label3)
+        vbox.addWidget(label4)
+        vbox.addWidget(label5)
+
+        central_widget . setLayout (vbox)
+
+        label1. setStyleSheet ("background-color: red; ")
+        label2. setStyleSheet ( "background-color: yellow; ")
+        label3. setStyleSheet ("background-color: green;")
+        label4. setStyleSheet ( "background-color: blue; ")
+        label5 . setStyleSheet ("background-color: purple; ")
+
+
+def main():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window. show()
+    sys. exit (app.exec_())
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
+
